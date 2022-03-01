e64f2d8a1a3e5950ddedab929d243d5d1055efd7
paskariu <rosine.95@gmx.de>
2022-03-01 19:45:56 +0100

    added download into seperate directory

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import AO3
+import os
 import re
+import inspect
 
 def main():
-  for work in getBookmarkedWorks(askForUsername()):
-    downloadWork(work)
+  downloadWorks(getBookmarkedWorks(askForUsername()))
 
 def askForUsername():
   print("Please enter your username:")
   return input()
 
 def getBookmarkedWorks(username):
+  print("Works will now be downloaded. This could take a while.")
   works=[]
   soup=AO3.User(username)._soup_bookmarks
   for temp in soup.find_all(class_="heading"):
@@ -17,9 +19,18 @@ def getBookmarkedWorks(username):
       works.append(AO3.Work((str(line).split('"')[1].split("/")[2])))
   return(works)
 
-def downloadWork(work):
-  with open(f"{work.title}.pdf", "wb") as file:
-    file.write(work.download("PDF"))
+def getBookmarkedWorksBySearch(username):
+  for a in AO3.Search(Bookmarker=username):
+    print(a.title)
+
+def downloadWorks(works):
+  directory="bookmarks"
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+  for work in works:
+    filename = work.title + ".pdf"
+    with open(os.path.join(directory,filename), "wb") as file:
+      file.write(work.download("PDF"))
 
 if __name__ == "__main__":
   main()
